1130d5328ed696f0c994925b3df2ca133dfe9e1c
John Lee <drumwolf@gmail.com>
2019-10-05 16:24:29 -0700

    first draft mockup of basic grid html and css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
+      <main className="md">
+        <div className="TTT-grid">
+          <div className="TTT-box"><span className="TTT-char">O</span></div>
+          <div className="TTT-box"><span className="TTT-char">X</span></div>
+          <div className="TTT-box"><span className="TTT-char">O</span></div>
+          <div className="TTT-box"><span className="TTT-char">X</span></div>
+          <div className="TTT-box"><span className="TTT-char">O</span></div>
+          <div className="TTT-box"><span className="TTT-char">X</span></div>
+          <div className="TTT-box"><span className="TTT-char">O</span></div>
+          <div className="TTT-box"><span className="TTT-char">X</span></div>
+          <div className="TTT-box"><span className="TTT-char">O</span></div>
+        </div>
+      </main>
     )
   }
 }
