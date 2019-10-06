bb50daf0126b532a323066c2c859e30f68e94a8c
John Lee <drumwolf@gmail.com>
2019-10-06 12:34:20 -0700

    defensive move method

--- src/TTTController.js
+++ src/TTTController.js
@@ -1,20 +1,45 @@
 class TTTController {
+  // each value represents an index of of TTT square
   corners = [0, 2, 6, 8];
+  combinations = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
 
   move(moveCount, squares) {
     this.squares = squares;
+    this.center  = squares[4];
+
     if (moveCount === 1) {
       return this.firstMove();
+    } else {
+      return this.defensiveMove();
     }
     return null;
   }
   firstMove() {
-    if (this.squares[4] === '') {
-      return 4;
-    } else {
-      const random = Math.floor( Math.random() * this.corners.length );
-      return this.corners[random];
+    return (this.center === '') ? 4 : this.availableCorner();
+  }
+  defensiveMove() {
+    for (let i = 0, combo; i < this.combinations.length; i++) {
+      combo = this.combinations[i];
+      const availableSquares = combo.filter( index => this.squares[index] !== 'X' );
+      if (availableSquares.length === 1 && this.squares[ availableSquares[0] ] === '' ) {
+        return availableSquares[0];
+      }
     }
+    return null;
+  }
+  availableCorner() {
+    const availableCorners = this.corners.filter( index => this.squares[index] === '' );
+    const random = Math.floor( Math.random() * availableCorners.length );
+    return this.corners[random];
   }
 }
 
