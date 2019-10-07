73b04fbb44d447cea2ca28b8de52e438ddefb6c1
John Lee <drumwolf@gmail.com>
2019-10-07 08:36:53 -0700

    combined offensive and defensive moves into one method, also use default move method

--- src/TTTController.js
+++ src/TTTController.js
@@ -15,26 +15,38 @@ class TTTController {
   move(moveCount, squares) {
     this.squares = squares;
     this.center  = squares[4];
-
-    if (moveCount === 1) {
-      return this.firstMove();
-    } else {
-      return this.defensiveMove();
-    }
-    return null;
+    return (moveCount === 1) ? this.firstMove() : this.nextMove();
+  }
+  defaultMove() {
+    const indices = [];
+    this.squares.forEach( (char, index) => {
+      if (char === '') { indices.push(index) }
+    });
+    const random = Math.floor( Math.random() * indices.length );
+    return indices[random];
   }
   firstMove() {
     return (this.center === '') ? 4 : this.availableCorner();
   }
-  defensiveMove() {
-    for (let i = 0, combo; i < this.combinations.length; i++) {
-      combo = this.combinations[i];
-      const availableSquares = combo.filter( index => this.squares[index] !== 'X' );
-      if (availableSquares.length === 1 && this.squares[ availableSquares[0] ] === '' ) {
-        return availableSquares[0];
+  nextMove() {
+    let defenseIndex = null;
+    for (let i = 0, XCount, OCount, emptyCount, emptyIndex; i < this.combinations.length; i++) {
+      let XCount = 0, OCount = 0, emptyCount = 0, emptyIndex = null;
+      this.combinations[i].forEach( index => {
+        if (this.squares[index] === 'X') { XCount++; }
+        if (this.squares[index] === 'O') { OCount++; }
+        if (this.squares[index] === '') { emptyCount++; emptyIndex = index; }
+      })
+      /* go on offensive immediately if there is an opportunity */
+      if (OCount === 2 && emptyCount === 1) {
+        return emptyIndex;
+      }
+      /* if app needs to make a defensive move, wait for the whole loop to see if there's also an opening for an attack */
+      else if (XCount === 2 && emptyCount === 1) {
+        defenseIndex = emptyIndex;
       }
     }
-    return null;
+    return (defenseIndex) ? defenseIndex : this.defaultMove();
   }
   availableCorner() {
     const availableCorners = this.corners.filter( index => this.squares[index] === '' );
